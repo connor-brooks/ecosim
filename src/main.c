d97ee9edd77bc0b987652c882efafa42a31e49c3
c <c@box>
2019-02-10 13:32:52 +0000

    Setup window

--- src/main.c
+++ src/main.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <GLFW/glfw3.h>
+#include <GL/gl.h>
 
 int
 main(int argc, char **argv)
 {
-  rintf("Hello World...\n");
+  GLFWwindow* window;
+
+  /* Initalize glfw */
+  if (!glfwInit())
+    return -1; //exit
+
+  /* Create window */
+  window = glfwCreateWindow(640, 480, "ecosim", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+
+  /* Set the windows context */
+  glfwMakeContextCurrent(window);
+
+  /* Main loop */
+  while(!glfwWindowShouldClose(window))
+  {
+    /* Render */
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    /* Swap buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for events */
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
