0644ecced404fbf0317b4dab49a24f8be78b4f92
c <mail@connor-brooks.com>
2019-04-09 13:25:17 +0100

    Removing old code

--- src/old/strings.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRINGS_H
-#define STRINGS_H
-char str_bad_key[] = "BAD KEYPRESS!";
-char str_mode_change[] = "Changing mode";
-char str_normal_mode[] = "Normal mode:";
-char str_insert_mode[] = "CMD mode:";
-char str_buffer_clear[] = "Buffer cleared";
-char str_execute_cmd[] = "Executing command";
-#endif
